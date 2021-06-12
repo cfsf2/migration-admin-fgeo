cea64d710dc5e45a88a0e8f6a936624d93f2bf14
Guillermo Albornoz <cachoalbornoz@gmail.com>
2021-06-12 08:42:40 -0300

    Fecha Actualizacion Modulo Admin

--- src/containers/DefaultLayout/DefaultFooter.js
+++ src/containers/DefaultLayout/DefaultFooter.js
@@ -3,35 +3,35 @@ import PropTypes from "prop-types";
 import { Container, Row, Col } from "reactstrap";
 
 const propTypes = {
-  children: PropTypes.node,
+    children: PropTypes.node,
 };
 
 const defaultProps = {};
 
 class DefaultFooter extends Component {
-  render() {
-    // eslint-disable-next-line
-    const { children, ...attributes } = this.props;
+    render() {
+        // eslint-disable-next-line
+        const { children, ...attributes } = this.props;
 
-    return (
-      <React.Fragment>
-        <Container>
-          <Row>
-            <Col>
-              <span style={{ float: "left" }}>
-                <a href="#">Farmageo</a> &copy; 2021
+        return (
+            <React.Fragment>
+                <Container>
+                    <Row>
+                        <Col>
+                            <span style={{ float: "left" }}>
+                                <a href="#">Farmageo</a> &copy; 2021
               </span>
-            </Col>
-            <Col>
-              <span style={{ float: "right" }}>
-                Últ. modif. 23/02/2021 16:00hs
+                        </Col>
+                        <Col>
+                            <span style={{ float: "right" }}>
+                                Últ. modif. 12/06/2021 08:00hs
               </span>
-            </Col>
-          </Row>
-        </Container>
-      </React.Fragment>
-    );
-  }
+                        </Col>
+                    </Row>
+                </Container>
+            </React.Fragment>
+        );
+    }
 }
 
 DefaultFooter.propTypes = propTypes;
