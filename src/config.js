bc5c3bdb35c5a991f6f6ccec0530aff3881bceed
Bruno Ramari <bjramariutn@hotmail.com>
2021-11-12 13:38:13 -0300

    cambio de config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const wp_api = "wp_api_auth_tkon/wp-json/wp/v2";
 const wp_api_auth = "https://api.testingdev.ml";
-//const farmageo_api = "https://api.testingdev.ml";
-const farmageo_api = "http://localhost:3110";
+const farmageo_api = "https://api.testingdev.ml";
+// const farmageo_api = "http://localhost:3110";
 const image_path_server = "https://farmageo2.s3.amazonaws.com/"; //incluir "/"
 
 export { wp_api, farmageo_api, wp_api_auth, image_path_server };
