bcb442697edc49c82a32bb5b513664ea02dc1beb
Bruno Ramari <bjramariutn@hotmail.com>
2022-01-06 16:02:38 -0300

    eliminar import csv alert

--- src/components/ImportarCsv.js
+++ src/components/ImportarCsv.js
@@ -1,51 +1,50 @@
-import React, { Component } from 'react'
-import { CSVReader } from 'react-papaparse'
- 
-const buttonRef = React.createRef()
- 
+import React, { Component } from "react";
+import { CSVReader } from "react-papaparse";
+
+const buttonRef = React.createRef();
+
 export default class ImportarCsv extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      disabled:true
-    }
+      disabled: true,
+    };
   }
-  handleOpenDialog =async (e) => {
+  handleOpenDialog = async (e) => {
     // Note that the ref is set async, so it might be null at some point
-    alert("asd")
+
     if (buttonRef.current) {
-      await buttonRef.current.open(e)
+      await buttonRef.current.open(e);
     }
-  }
- 
+  };
+
   handleOnFileLoad = (data) => {
-    this.props.handleResponse(data)
+    this.props.handleResponse(data);
     //console.log('---------------------------')
     //console.log(data)
     //console.log('---------------------------')
-  }
- 
+  };
+
   handleOnError = (err, file, inputElem, reason) => {
-    console.log(err)
-  }
- 
+    console.log(err);
+  };
+
   handleOnRemoveFile = (data) => {
     //console.log('---------------------------')
     //console.log(data)
     //console.log('---------------------------')
-    
-  }
- 
+  };
+
   handleRemoveFile = (e) => {
     if (buttonRef.current) {
-      buttonRef.current.removeFile(e)
+      buttonRef.current.removeFile(e);
     }
-  }
- 
-   componentDidUpdate(prevProps, prevState) {
+  };
+
+  componentDidUpdate(prevProps, prevState) {
     if (prevProps.disabled !== this.props.disabled) {
-       this.setState({
-        disabled: this.props.disabled
+      this.setState({
+        disabled: this.props.disabled,
       });
     }
   }
@@ -63,21 +62,21 @@ export default class ImportarCsv extends Component {
         {({ file }) => (
           <aside
             style={{
-              display: 'flex',
-              flexDirection: 'row',
-              marginBottom: 10
+              display: "flex",
+              flexDirection: "row",
+              marginBottom: 10,
             }}
           >
             <button
-              type='button'
+              type="button"
               onClick={this.handleOpenDialog}
               style={{
                 borderRadius: 0,
                 marginLeft: 0,
                 marginRight: 0,
-                width: '40%',
+                width: "40%",
                 paddingLeft: 0,
-                paddingRight: 0
+                paddingRight: 0,
               }}
               className="btn btn-dark"
               disabled={this.state.disabled}
@@ -87,15 +86,15 @@ export default class ImportarCsv extends Component {
             <div
               style={{
                 borderWidth: 1,
-                borderStyle: 'solid',
-                borderColor: '#ccc',
-               // height: 45,
+                borderStyle: "solid",
+                borderColor: "#ccc",
+                // height: 45,
                 lineHeight: 2.5,
-               // marginTop: 5,
-               // marginBottom: 5,
+                // marginTop: 5,
+                // marginBottom: 5,
                 paddingLeft: 13,
                 paddingTop: 3,
-                width: '60%'
+                width: "60%",
               }}
             >
               {file && file.name}
@@ -116,6 +115,6 @@ export default class ImportarCsv extends Component {
           </aside>
         )}
       </CSVReader>
-    )
+    );
   }
 }
