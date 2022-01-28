541fa204d59b20e7d3594473519f2f00772b36c7
Bruno Ramari <bjramariutn@hotmail.com>
2022-01-28 12:33:07 -0300

    bucket de testing

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 const wp_api = "wp_api_auth_tkon/wp-json/wp/v2";
 const wp_api_auth = "https://api.testingdev.ml";
-const farmageo_api = "https://api.testingdev.ml";
-//const farmageo_api = "http://localhost:3110";
-const image_path_server = "https://sync-farmageo2.s3.amazonaws.com/";
+//const farmageo_api = "https://api.testingdev.ml";
+const farmageo_api = "http://localhost:3110";
+//const image_path_server = "https://sync-farmageo2.s3.amazonaws.com/";
+const image_path_server = "https://testing-farmageo.s3.amazonaws.com/";
 export { wp_api, farmageo_api, wp_api_auth, image_path_server };
