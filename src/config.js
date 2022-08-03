17311bfbde95080dd69c353bdd4688e028dc7110
Bruno Ramari <bjramari@hotmail.com>
2022-08-03 12:54:18 -0300

    envlocal

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const wp_api = "wp_api_auth_tkon/wp-json/wp/v2";
 const wp_api_auth = "https://api.testingdev.ga";
-const farmageo_api = process.env.FARMAGEO_API;
+const farmageo_api = process.env.REACT_APP_FARMAGEO_API;
 //const farmageo_api = "http://localhost:3110";
 const image_path_server = "https://sync-farmageo2.s3.amazonaws.com/";
 //const image_path_server = "https://testing-farmageo.s3.amazonaws.com/";
