0baa04b69b9c33a36a915d2fcd76ae54e856db9b
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-08-23 13:22:17 -0300

    autocompletar funciones

--- src/views/Pantalla/components/Autocompletar.js
+++ src/views/Pantalla/components/Autocompletar.js
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
+import FuncionesContext from "../context/FuncionesContext";
+import ListadoContext from "../Listado/context/ListadoContext";
 
-const Autocompletar = () => {
+const Autocompletar = ({ cab, data }) => {
+  const { superSubmit } = useContext(FuncionesContext);
+
+  const { ListadoDispatch } = useContext(ListadoContext);
+
   const [value, setValue] = React.useState();
   const [inputValue, setInputValue] = React.useState("");
+
+  const handleChangeValue = (newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <>
       <Autocomplete
         value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
+        onChange={(event, newValue) => handleChangeValue(newValue)}
         inputValue={inputValue}
         onInputChange={(event, newInputValue) => {
           setInputValue(newInputValue);
         }}
-        sx={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label="Controllable" />}
+        sx={{ width: 400 }}
+        options={cab.opciones}
+        renderInput={(params) => (
+          <TextField {...params} label={cab.select_label_null} />
+        )}
       />
     </>
   );
