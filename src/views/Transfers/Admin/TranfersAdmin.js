9d2631b7ab28705de9cbef73fb9f78c0eaa43c96
Bruno Ramari <bjramari@hotmail.com>
2022-10-12 16:00:33 -0300

    TransferAdmin a componente funcional

--- src/views/Transfers/Admin/TranfersAdmin.js
+++ src/views/Transfers/Admin/TranfersAdmin.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment, useState, useEffect } from "react";
 import {
   Button,
   Card,
@@ -20,20 +20,15 @@ import {
   GET_DROGUERIAS,
 } from "../../../redux/actions/transfersActions";
 
-class TransfersAdmin extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      transfer: null,
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
+const TransfersAdmin = (props) => {
+  const [transfer, setTransfer] = useState(null);
+  const [enviandoMail, setEnviando] = useState(false);
 
-  componentDidMount() {
-    this.props.GET_TRANSFERS();
-  }
+  useEffect(() => {
+    props.GET_TRANSFERS();
+  }, []);
 
-  async handleInputChange(event) {
+  async function handleInputChange(event) {
     const target = event.nativeEvent.target;
     const value = target.type === "checkbox" ? target.checked : target.value;
     const name = target.name;
@@ -46,181 +41,187 @@ class TransfersAdmin extends Component {
     });
   }
 
-  render() {
-    const { transfers } = this.props.tranfersReducer;
-    return (
-      <div className="animated fadeIn">
-        <Row>
-          <Col xs="12" sm="12">
-            <Row>
-              <Col>
-                <Card>
-                  <CardHeader>
-                    <b>Listado de transfers</b>
-                  </CardHeader>
-                  <CardBody>
-                    <Row>
-                      <Col xs="12" md="4" style={{ marginBottom: 10 }}>
-                        <Input
-                          type="text"
-                          placeholder="buscar transfer..."
-                          name="filtro"
-                        />
-                      </Col>
-                    </Row>
-                    <hr />
-                    <div className="table-responsive table-striped table-fix">
-                      <table className="table ">
-                        <thead className="bg-secondary">
-                          <tr>
-                            <th>Código</th>
-                            <th>Estado</th>
-                            <th>Email destinatario</th>
-                            <th>Fecha</th>
-                            <th>Farmacia</th>
-                            <th>Farmacia codigo</th>
-                            <th>Laboratorio</th>
-                            <th>Drogueria</th>
-                            <th>Acciones</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {transfers.map((obj, index) => {
-                            return (
-                              <tr key={index}>
-                                <td>{obj.codigo_transfer}</td>
-                                <td>{obj.estado}</td>
-                                <td>{obj.email_destinatario}</td>
-                                <td>{obj.fecha.substring(0, 10)}</td>
-                                <td>{obj.farmacia_nombre}</td>
-                                <td>{obj.farmacia_id}</td>
-                                <td>{obj.laboratorio_id}</td>
-                                <td>{obj.drogueria_id}</td>
-                                <td>
-                                  <button
-                                    type="button"
-                                    className="btn btn-secondary"
-                                    data-toggle="modal"
-                                    data-target=".bd-example-modal-lg"
-                                    onClick={() => {
-                                      this.setState({ transfer: obj });
-                                    }}
-                                  >
-                                    ver
-                                  </button>
-                                </td>
-                              </tr>
-                            );
-                          })}
-                        </tbody>
-                      </table>
-                    </div>
-                    <div
-                      className="modal fade bd-example-modal-lg"
-                      tabIndex="-1"
-                      role="dialog"
-                      aria-labelledby="myLargeModalLabel"
-                      aria-hidden="true"
-                    >
-                      <div className="modal-dialog modal-lg">
-                        <div className="modal-content">
-                          {this.state.transfer === null ? null : (
-                            <Row>
-                              <Col xs="12" md="12">
-                                <Card>
-                                  <CardHeader>
-                                    <Row>
-                                      <Col>
-                                        Transfer:{" "}
-                                        {this.state.transfer.codigo_transfer}
-                                      </Col>
-                                      <Col>
-                                        <Button
-                                          className="close"
-                                          data-dismiss="modal"
-                                        >
-                                          {" "}
-                                          X{" "}
-                                        </Button>
-                                      </Col>
-                                    </Row>
-                                  </CardHeader>
-                                  <CardBody>
-                                    <Row className="my-2">
-                                      <div className="table-responsive">
-                                        <table className="table table-striped">
-                                          <thead className="bg-dark">
-                                            <tr>
-                                              <th>sku</th>
-                                              <th>Producto</th>
-                                              <th>Cantidad</th>
-                                              <th>Presentacion</th>
-                                              <th>Observación</th>
-                                            </tr>
-                                          </thead>
-                                          <tbody>
-                                            {this.state.transfer
-                                              .productos_solicitados.length > 0
-                                              ? this.state.transfer.productos_solicitados.map(
-                                                  (linea, index) => {
-                                                    console.log(linea);
-                                                    return (
-                                                      <tr>
-                                                        <td>
-                                                          {
-                                                            linea.codigo_producto
-                                                          }
-                                                        </td>
-                                                        <td>{linea.nombre}</td>
-                                                        <td>
-                                                          {linea.cantidad}
-                                                        </td>
-                                                        <td>
-                                                          {linea.presentacion}
-                                                        </td>
-                                                        <td>
-                                                          {linea.observaciones}
-                                                        </td>
-                                                      </tr>
-                                                    );
-                                                  }
-                                                )
-                                              : null}
-                                          </tbody>
-                                        </table>
-                                      </div>
-                                    </Row>
-                                    <hr />
-                                    <br />
-                                    <Row>
-                                      <Col className="mt-3">
-                                        <Button
-                                          className="close"
-                                          data-dismiss="modal"
-                                        >
-                                          {" "}
-                                          X{" "}
-                                        </Button>
-                                      </Col>
-                                    </Row>
-                                  </CardBody>
-                                </Card>
-                              </Col>
-                            </Row>
-                          )}
-                        </div>
+  console.log(props.transferReducer);
+
+  const { transfers } = props.tranfersReducer;
+  return (
+    <div className="animated fadeIn">
+      <Row>
+        <Col xs="12" sm="12">
+          <Row>
+            <Col>
+              <Card>
+                <CardHeader>
+                  <b>Listado de transfers</b>
+                </CardHeader>
+                <CardBody>
+                  <Row>
+                    <Col xs="12" md="4" style={{ marginBottom: 10 }}>
+                      <Input
+                        type="text"
+                        placeholder="buscar transfer..."
+                        name="filtro"
+                      />
+                    </Col>
+                  </Row>
+                  <hr />
+                  <div className="table-responsive table-striped table-fix">
+                    <table className="table ">
+                      <thead className="bg-secondary">
+                        <tr>
+                          <th>Código</th>
+                          <th>Estado</th>
+                          <th>Email destinatario</th>
+                          <th>Fecha</th>
+                          <th>Farmacia</th>
+                          <th>Farmacia codigo</th>
+                          <th>Laboratorio</th>
+                          <th>Drogueria</th>
+                          <th>Acciones</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {transfers.map((obj, index) => {
+                          return (
+                            <tr key={index}>
+                              <td>{obj.codigo_transfer}</td>
+                              <td>{obj.estado}</td>
+                              <td>{obj.email_destinatario}</td>
+                              <td>{obj.fecha.substring(0, 10)}</td>
+                              <td>{obj.farmacia_nombre}</td>
+                              <td>{obj.farmacia_id}</td>
+                              <td>{obj.laboratorio_id}</td>
+                              <td>{obj.drogueria_id}</td>
+                              <td>
+                                <button
+                                  type="button"
+                                  className="btn btn-secondary"
+                                  data-toggle="modal"
+                                  data-target=".bd-example-modal-lg"
+                                  onClick={() => {
+                                    setTransfer({ transfer: obj });
+                                  }}
+                                >
+                                  ver
+                                </button>
+                                <button
+                                  type="button"
+                                  className="btn btn-primary"
+                                  onClick={() => {
+                                    setTransfer({ transfer: obj });
+                                  }}
+                                >
+                                  {enviandoMail
+                                    ? "Enviando..."
+                                    : "Reenviar Email"}
+                                </button>
+                              </td>
+                            </tr>
+                          );
+                        })}
+                      </tbody>
+                    </table>
+                  </div>
+                  <div
+                    className="modal fade bd-example-modal-lg"
+                    tabIndex="-1"
+                    role="dialog"
+                    aria-labelledby="myLargeModalLabel"
+                    aria-hidden="true"
+                  >
+                    <div className="modal-dialog modal-lg">
+                      <div className="modal-content">
+                        {transfer === null ? null : (
+                          <Row>
+                            <Col xs="12" md="12">
+                              <Card>
+                                <CardHeader>
+                                  <Row>
+                                    <Col>
+                                      Transfer: {transfer.codigo_transfer}
+                                    </Col>
+                                    <Col>
+                                      <Button
+                                        className="close"
+                                        data-dismiss="modal"
+                                      >
+                                        {" "}
+                                        X{" "}
+                                      </Button>
+                                    </Col>
+                                  </Row>
+                                </CardHeader>
+                                <CardBody>
+                                  <Row className="my-2">
+                                    <div className="table-responsive">
+                                      <table className="table table-striped">
+                                        <thead className="bg-dark">
+                                          <tr>
+                                            <th>sku</th>
+                                            <th>Producto</th>
+                                            <th>Cantidad</th>
+                                            <th>Presentacion</th>
+                                            <th>Observación</th>
+                                          </tr>
+                                        </thead>
+                                        <tbody>
+                                          {transfer.productos_solicitados
+                                            .length > 0
+                                            ? transfer.productos_solicitados.map(
+                                                (linea, index) => {
+                                                  console.log(linea);
+                                                  return (
+                                                    <tr>
+                                                      <td>
+                                                        {linea.codigo_producto}
+                                                      </td>
+                                                      <td>{linea.nombre}</td>
+                                                      <td>{linea.cantidad}</td>
+                                                      <td>
+                                                        {linea.presentacion}
+                                                      </td>
+                                                      <td>
+                                                        {linea.observaciones}
+                                                      </td>
+                                                    </tr>
+                                                  );
+                                                }
+                                              )
+                                            : null}
+                                        </tbody>
+                                      </table>
+                                    </div>
+                                  </Row>
+                                  <hr />
+                                  <br />
+                                  <Row>
+                                    <Col className="mt-3">
+                                      <Button
+                                        className="close"
+                                        data-dismiss="modal"
+                                      >
+                                        {" "}
+                                        X{" "}
+                                      </Button>
+                                    </Col>
+                                  </Row>
+                                </CardBody>
+                              </Card>
+                            </Col>
+                          </Row>
+                        )}
                       </div>
                     </div>
-                  </CardBody>
-                </Card>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
-}
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   return { tranfersReducer: state.tranfersReducer };
