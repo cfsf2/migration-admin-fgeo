000b4e230adacba5b017410bb1d42feabec21bc8
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-11-18 17:00:21 -0300

    ajuste font size Default.js en listado

--- src/views/Pantalla/Listado/components/columnas/Default.js
+++ src/views/Pantalla/Listado/components/columnas/Default.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 const Default = ({ data, cab, hijos, campokey }) => {
+  const condcionBadge = cab.id_a === "T_COL_ID_TRANSFER" && "0.9em";
+
   return (
     <div
       className={data[cab.id_a + "_className"]}
-      style={{ textAlign: "center" }}
+      style={{ textAlign: "center", fontSize: condcionBadge }}
     >
       {data[campokey]}
       {hijos}
@@ -18,7 +20,7 @@ export const Code = ({ data, cab, hijos, campokey }) => {
   return (
     <code
       className={data[cab.id_a + "_className"]}
-      style={{ textAlign: "center" }}
+      style={{ textAlign: "center", color: "red" }}
     >
       {data[campokey]}
       {hijos}
