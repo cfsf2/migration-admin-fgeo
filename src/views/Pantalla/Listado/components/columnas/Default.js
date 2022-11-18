ea955999762ea515c0be5bc6836fc2639c166f5f
Bruno Ramari <bjramari@hotmail.com>
2022-11-18 13:31:21 -0300

    fixes classname clolumna_simple

--- src/views/Pantalla/Listado/components/columnas/Default.js
+++ src/views/Pantalla/Listado/components/columnas/Default.js
@@ -3,7 +3,7 @@ import React from "react";
 const Default = ({ data, cab, hijos, campokey }) => {
   return (
     <div
-      className={data[cab.id_a + "_className"] + cab.className}
+      className={data[cab.id_a + "_className"]}
       style={{ textAlign: "center" }}
     >
       {data[campokey]}
@@ -17,7 +17,7 @@ export default Default;
 export const Code = ({ data, cab, hijos, campokey }) => {
   return (
     <code
-      className={data[cab.id_a + "_className"] + cab.className}
+      className={data[cab.id_a + "_className"]}
       style={{ textAlign: "center" }}
     >
       {data[campokey]}
