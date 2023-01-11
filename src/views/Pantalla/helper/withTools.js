ae11a6fd9738da01473917dd10daad0f0ce6e322
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2023-01-11 14:25:11 -0300

    withTools_:conditional styling tooltip

--- src/views/Pantalla/helper/withTools.js
+++ src/views/Pantalla/helper/withTools.js
@@ -1,6 +1,4 @@
-import React, {
-  useEffect,
-} from "react";
+import React, { useEffect } from "react";
 
 export const withTools = (Componente) => (props) => {
   const { data, cab, indiceData, id_elemento } = props;
@@ -9,17 +7,32 @@ export const withTools = (Componente) => (props) => {
     const g = document.getElementById(id_elemento)?.parentNode;
     if (g) {
       if (cab.tooltip_texto) {
-       
         const id_tooltip = id_elemento + "_tooltip" + indiceData;
 
         const tooltip = document.createElement("span");
         tooltip.id = id_tooltip;
         tooltip.appendChild(
-          document.createTextNode(`${data[cab.id_a + "_tooltip_texto"] ?? cab.tooltip_texto}`)
+          document.createTextNode(
+            `${data[cab.id_a + "_tooltip_texto"] ?? cab.tooltip_texto}`
+          )
         );
-        tooltip.classList.add("Ftooltip") // listado.scss
 
+        tooltip.classList.add("Ftooltip"); // listado.scss
         g.style.position = "relative";
+
+        if (cab.tooltip_class === "okey") {
+          tooltip.style.backgroundColor = "limegreen";
+          tooltip.style.borderBottomColor = "limegreen";
+          tooltip.classList.add("Ftooltip_okey");
+        }
+
+        if (cab.tooltip_class === "danger") {
+          tooltip.style.backgroundColor = "tomato";
+          tooltip.style.borderBottomColor = "tomato";
+          tooltip.style.color = "black";
+          tooltip.classList.add("Ftooltip_danger");
+        }
+
         g.addEventListener("mouseenter", (e) => {
           e.stopPropagation();
           g.appendChild(tooltip);
