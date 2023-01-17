effeaac9d1230f9036b5126e80d6fb6ba82bb63e
Bruno Ramari <bjramari@hotmail.com>
2023-01-17 09:56:25 -0300

    tooltip no se va de pantalla

--- src/views/Pantalla/helper/withTools.js
+++ src/views/Pantalla/helper/withTools.js
@@ -1,22 +1,39 @@
-import React, {
-  useEffect,
-} from "react";
+import React, { useEffect } from "react";
 
 export const withTools = (Componente) => (props) => {
   const { data, cab, indiceData, id_elemento } = props;
 
   useEffect(() => {
     const g = document.getElementById(id_elemento)?.parentNode;
-    console.log(g, id_elemento)
+
     if (g) {
       if (cab.tooltip_texto) {
-       
         const id_tooltip = id_elemento + "_tooltip" + indiceData;
+
+        const observer = new IntersectionObserver((entries, observer) => {
+          entries.forEach((e) => {
+            const rightLimit = e.rootBounds.right;
+            const rightObj = e.boundingClientRect.right;
+            const leftLimit = e.rootBounds.left;
+            const leftObj = e.boundingClientRect.left;
+
+            if (rightObj > rightLimit) {
+              const t = document.getElementById(id_tooltip);
+              t.style.left = `-${rightObj - rightLimit - 30}px`
+            }
+            if (leftObj < leftLimit) {
+              const t = document.getElementById(id_tooltip);
+              t.style.left = `${leftObj - leftLimit + 30}px`
+            }
+          });
+        });
 
         const tooltip = document.createElement("span");
         tooltip.id = id_tooltip;
         tooltip.appendChild(
-          document.createTextNode(`${data[cab.id_a + "_tooltip_texto"] ?? cab.tooltip_texto}`)
+          document.createTextNode(
+            `${data[cab.id_a + "_tooltip_texto"] ?? cab.tooltip_texto}`
+          )
         );
 
         tooltip.classList.add("Ftooltip"); // Pantalla.scss
@@ -24,11 +41,14 @@ export const withTools = (Componente) => (props) => {
         g.addEventListener("mouseenter", (e) => {
           e.stopPropagation();
           g.appendChild(tooltip);
+
+          observer.observe(tooltip);
         });
 
         g.addEventListener("mouseleave", (e) => {
           e.stopPropagation();
           const t = document.getElementById(id_tooltip);
+          observer.unobserve(t);
 
           if (t) {
             t.remove();
