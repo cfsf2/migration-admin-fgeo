4509ff64ce483af50adbff6e80e532831362edc8
Bruno Ramari <bjramari@hotmail.com>
2023-07-18 13:05:24 -0300

    round

--- src/views/Pantalla/Listado/components/columnas/Default.js
+++ src/views/Pantalla/Listado/components/columnas/Default.js
@@ -3,8 +3,9 @@ import React from "react";
 const Default = ({ data, cab, hijos, campokey, indiceData, id_elemento }) => {
   const condcionBadge = cab.id_a === "T_COL_ID_TRANSFER" && "0.9em";
 
-  const regex = /^[0-9]+\.?[0-9]*$/;
-  const number = regex.test(data[campokey]); // true
+  const valor = cab.round
+    ? Number(data[campokey]).toFixed(cab.round)
+    : data[campokey];
 
   return (
     <div
@@ -12,7 +13,7 @@ const Default = ({ data, cab, hijos, campokey, indiceData, id_elemento }) => {
       className={data[cab.id_a + "_className"]}
       style={{ textAlign: cab.align ?? "center", fontSize: condcionBadge }}
     >
-      {number ? data[campokey].toFixed(2) : data[campokey]}
+      {valor}
       {hijos}
     </div>
   );
