14d6e037f66d3bc6b2d9abbaf162e468cbcf0206
Bruno Ramari <bjramari@hotmail.com>
2024-05-15 11:08:37 -0300

    img ruta completa

--- src/views/Pantalla/Listado/components/columnas/Imagen.js
+++ src/views/Pantalla/Listado/components/columnas/Imagen.js
@@ -3,24 +3,17 @@ import { image_path_server } from "../../../../../config";
 
 const Imagen = ({ data, cab, hijos, campokey, indiceData, id_elemento }) => {
   const classNames = data[cab.id_a + "_className"] ?? cab.className;
-  if (cab.imagen_local === "s") {
-    const imgurl = "/" + data[campokey].replace(/\.\.\//g, "");
-    return (
-      <div
-        id={id_elemento}
-        style={{ width: "100%", textAlign: "center" }}
-        className={classNames}
-      >
-        <img
-          src={imgurl}
-          style={{ textAlign: "center" }}
-          alt={cab.nombre + " " + campokey}
-          width={cab.image_width}
-        />
-        {hijos}
-      </div>
-    );
-  }
+
+  const imgurl = () => {
+    if (cab.imagen_local === "s") {
+      return "/" + data[campokey].replace(/\.\.\//g, "");
+    }
+    if (cab.imagen_ruta_completa === "s") {
+      return data[campokey];
+    }
+    return image_path_server + data[campokey];
+  };
+
   return (
     <div
       id={id_elemento}
@@ -28,7 +21,7 @@ const Imagen = ({ data, cab, hijos, campokey, indiceData, id_elemento }) => {
       className={classNames}
     >
       <img
-        src={image_path_server + data[campokey]}
+        src={imgurl()}
         style={{ textAlign: "center" }}
         alt={cab.nombre + " " + campokey}
         width={cab.image_width}
