04134233b9e53b984ed16b1643d267e8fdc737c1
Bruno Ramari <bjramari@hotmail.com>
2024-05-15 09:53:11 -0300

    imagenes locales

--- src/views/Pantalla/Listado/components/columnas/Imagen.js
+++ src/views/Pantalla/Listado/components/columnas/Imagen.js
@@ -3,6 +3,25 @@ import { image_path_server } from "../../../../../config";
 
 const Imagen = ({ data, cab, hijos, campokey, indiceData, id_elemento }) => {
   const classNames = data[cab.id_a + "_className"] ?? cab.className;
+  if (cab.imagen_local === "s") {
+    const imgurl = "/" + data[campokey].replace(/\.\.\//g, "");
+    console.log(imgurl);
+    return (
+      <div
+        id={id_elemento}
+        style={{ width: "100%", textAlign: "center" }}
+        className={classNames}
+      >
+        <img
+          src={imgurl}
+          style={{ textAlign: "center" }}
+          alt={cab.nombre + " " + campokey}
+          width={cab.image_width}
+        />
+        {hijos}
+      </div>
+    );
+  }
   return (
     <div
       id={id_elemento}
